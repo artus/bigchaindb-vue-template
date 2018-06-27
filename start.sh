b7203ba77479a7ff33db4d46c35bd00fdba885f6
artus <artusvranken@gmail.com>
2018-06-27 16:19:06 +0200

    Updated docker-compose.yml.

--- start.sh
+++ start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-npm run build &&
-git clone https://github.com/bigchaindb/bigchaindb &&
+npm run build;
+git clone https://github.com/bigchaindb/bigchaindb;
+
+if [ -d "bigchaindb" ]; then
+  cd bigchaindb;
+  git pull;
+  cd ..;
+else 
+  git clone https://github.com/bigchaindb/bigchaindb
+fi
+
+mv ./docker-compose.yml ./bigchaindb/docker-compose.yml;
+
 docker-compose build -d bigchaindb &&
 docker-compose run -d bdb;
