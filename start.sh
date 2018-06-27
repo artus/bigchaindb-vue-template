633296d976191979aef151dacbdacfec282d6bbb
artusvranken <artusvranken@gmail.com>
2018-06-27 16:22:43 +0200

    Updated startup script.

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+npm install;
 npm run build;
 git clone https://github.com/bigchaindb/bigchaindb;
 
 if [ -d "bigchaindb" ]; then
-  cd bigchaindb;
-  git pull;
-  cd ..;
-else 
-  git clone https://github.com/bigchaindb/bigchaindb
-fi
+  rm -rf bigchaindb;
+fi;
 
-mv ./docker-compose.yml ./bigchaindb/docker-compose.yml;
+git clone https://github.com/bigchaindb/bigchaindb
+
+cp -f ./docker-compose.yml ./bigchaindb/docker-compose.yml;
 
 docker-compose build -d bigchaindb &&
 docker-compose run -d bdb;
